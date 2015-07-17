7e9023b562180f6a4d5c5e5e37ecce18d2000a6c
Amritanshu Joshi <amrijosh@indiana.edu>
2015-07-16 23:44:09 -0400

    Lab Codes
    Week 1 through Week 4

---
+++ Lab Codes/Week 1/Problem Set 1 - Counting Bob.py
@@ -0,0 +1,4 @@
+numberOfTimes = 0
+for 'bob' in 'azcbobobegghakl':
+    numberOfTimes += 1
+print 'Number of times bob occurs is: ' + str(numberOfTimes)
---
+++ Lab Codes/Week 1/Problem Set 1 - Counting Vowels.py
@@ -0,0 +1,5 @@
+numVowels = 0
+for letter in s:
+    if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+        numVowels += 1
+print 'Number of vowels: ' + str(numVowels)
